60cf2cc3a3b575fa2e9e318459d5a4e9f7666789
0cl132 <akiuiepro@gmail.com>
2022-02-10 12:42:51 +0100

    Project RecipIT

--- RecipitApp.swift
+++ RecipitApp.swift
@@ -1,17 +1,19 @@
 //
-//  RecipitApp.swift
-//  Recipit
+//  RecipITApp.swift
+//  RecipIT
 //
-//  Created by Carolane on 10/02/2022.
+//  Created by Carolane on 07/02/2022.
 //
 
 import SwiftUI
 
 @main
-struct RecipitApp: App {
+struct RecipITApp: App {
+    @StateObject var recipeViewModel = RecipeViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(recipeViewModel)
         }
     }
 }
